c36b73d4755cdf736b4f9e45a87a8fb58d08f771
Frederik Ludwigs <kuchenmann93@gmail.com>
2023-05-28 11:11:00 +0200

    added template file for a R script

---
+++ Template/code/R_Template.R
@@ -0,0 +1,46 @@
+## General:
+#  (1) Tools > Global Options > Code > Display > MarginColumn = 100
+#  (2) Tools > Global Options > Apperance > Cobalt
+#  (3) Unifrom variable-names: "uniform_variable_names"
+#       - all lower case
+#       - word splits with underscores
+"
+Date: 
+Name:
+Partner:
+
+What is this script doing?! 
+What is the overall goal?!
+
+(1) Chapter1 Topic
+    - Detail 1
+    - Detail 2
+    - ...
+(2) Chapter2 Topic
+    - Detail 1
+    - Detail 2
+"
+# [0] Set WD, load packages, define variables & functions                                       ----
+# 0-1 WD
+
+# 0-2 Packages
+
+# 0-3 Variables
+
+# 0-4 Functions
+
+# [1] Chapter1                                                                                  ----
+# 1-1 SubChapter1
+# 1-1-1 SubSubChapter1
+# 1-1-1-1 SubSubSubChapter1
+sapply(cars$speed, function(x) {
+  # --1 First step in the loop/ apply/ ...
+  
+  # --2 Second step in the loop/ apply/ ...
+})
+
+
+# [2] Chapter2                                                                                  ----
+# 2-1 SubChapter1
+# 2-1-1 SubSubChapter1
+# 2-1-1-1 SubSubSubChapter1
