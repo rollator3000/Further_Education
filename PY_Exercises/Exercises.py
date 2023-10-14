58572b7e14d7e2d11a157842327bfa7e40553ca5
Frederik Ludwigs <kuchenmann93@gmail.com>
2023-10-14 12:12:18 +0200

    added new riddle :-)

--- PY_Exercises/Exercises.py
+++ PY_Exercises/Exercises.py
@@ -43,3 +43,42 @@ def check_two_sum(numbers, k):
 # 1-3 Check it:
 check_two_sum(numbers = [2, 7, 5, 11, -7, 6, -131], k = 1)    # Should be FALSE
 check_two_sum(numbers = [2, 7, 5, 11, -7, 6, -131], k = -129) # Should be TRUE
+
+# %% (2) 
+"""
+- This problem was asked by Uber
+- Given an array of integers, return a new array such that each element at 
+  index i of the new array is the product of all the numbers in the original
+  array except the one at i.
+- For example, if our input was [1, 2, 3, 4, 5], the expected output would be 
+ [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would 
+ be [2, 3, 6].
+"""
+# 2-1 Define a list 
+int_list = [3, 2, 1]
+
+# 2-2 Write a function
+def list_product(int_list):
+    
+    # 1) Empty list to fill products of all list elements except 'i' 
+    return_list = []
+    
+    # 2) Loop over indeces in int_list
+    for curr_ind in range(0, len(int_list)):
+        
+        # 2-1 Take all elements from 'int_list' except the one on 'curr_ind'
+        elements_except_curr_ind = int_list[:curr_ind] + int_list[curr_ind+1:]
+        
+        # 2-2 Get the product of all elements in elements_except_curr_ind
+        curr_result = 1
+        for x in elements_except_curr_ind:
+            curr_result = curr_result * x
+         
+        return_list.append(curr_result)
+        
+    # 3) Return the new list 
+    return return_list
+        
+# 2-3 Check it:
+list_product(int_list = [1, 2, 3, 4, 5])  # Should be: [120, 60, 40, 30, 24]
+list_product(int_list = [3, 2, 7, 2, -2]) # Should be: [-56, -84, -24, -84, 84]
